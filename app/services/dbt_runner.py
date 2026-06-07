6ba9899f590885c79e3d2fd3190ca007af507758
Rajat Rampal <rajatrampal@mac.home>
2026-06-06 23:21:14 -0500

    feat: add dbt runner and subprocess wrapper

---
+++ app/services/dbt_runner.py
@@ -0,0 +1,32 @@
+import subprocess
+import json 
+from pathlib import Path 
+
+
+DBT_PROJECT_DIR = Path("dbt_project")
+
+
+def run_dbt_command(command: list[str]) -> dict:
+
+    result= subprocess.run(
+        command,
+        capture_output=True,
+        text=True,
+        cwd=DBT_PROJECT_DIR
+    )
+
+    return{
+        "success": result.returncode==0,
+        "stdout": result.stdout,
+        "stderr": result.stderr
+    }
+
+#wrapper functions
+def dbt_run() -> dict:
+    return run_dbt_command(["dbt","run","--profiles-dir","."])
+
+def dbt_test() -> dict:
+    return run_dbt_command(["dbt","test","--profiles-dir","."])
+
+def dbt_ls() -> dict:
+    return run_dbt_command(["dbt","ls","--profiles-dir","."])
