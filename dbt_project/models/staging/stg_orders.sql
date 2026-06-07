9ad87844dc2752d5268a8c840ee1550bb6bb2193
Rajat Rampal <rajatrampal@mac.home>
2026-06-06 22:39:53 -0500

    feat: add dbt project with duckdb adapater and stg_orders model

---
+++ dbt_project/models/staging/stg_orders.sql
@@ -0,0 +1,4 @@
+SELECT 
+    1 AS order_id,
+    'pending' AS status,
+    100.00 AS amount 
---
+++ dbt_project/target/run/dbt_monitor/models/staging/stg_orders.sql
@@ -0,0 +1,8 @@
+
+  
+  create view "dev"."main"."stg_orders__dbt_tmp" as (
+    SELECT 
+    1 AS order_id,
+    'pending' AS status,
+    100.00 AS amount
+  );
